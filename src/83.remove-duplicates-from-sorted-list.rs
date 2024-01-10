79585e5645838315bc0f37ab06494ef969644074
LHD <sunupdong@gmail.com>
2024-01-10 16:37:09 +0800

    Create 83.remove-duplicates-from-sorted-list.rs

---
+++ src/83.remove-duplicates-from-sorted-list.rs
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=83 lang=rust
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (51.85%)
+ * Likes:    8345
+ * Dislikes: 284
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given the head of a sorted linked list, delete all duplicates such that each
+ * element appears only once. Return the linked list sorted as well.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is in the range [0, 300].
+ * -100 <= Node.val <= 100
+ * The list is guaranteed to be sorted in ascending order.
+ *
+ *
+ */
+
+// @lc code=start
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return None;
+        }
+        let mut h = head;
+        let mut p1 = h.as_mut().unwrap();
+        while let Some(p2) = p1.next.as_mut() {
+            if p1.val == p2.val {
+                p1.next = p2.next.take();
+            } else {
+                p1 = p1.next.as_mut().unwrap();
+            }
+        }
+        h
+    }
+}
+// @lc code=end
