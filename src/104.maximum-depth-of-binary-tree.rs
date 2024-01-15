5b9d5d69e4f2115fa66bea946621d676df496326
LHD <sunupdong@gmail.com>
2024-01-15 23:45:33 +0800

    Create 104.maximum-depth-of-binary-tree.rs

---
+++ src/104.maximum-depth-of-binary-tree.rs
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=104 lang=rust
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (74.84%)
+ * Likes:    12344
+ * Dislikes: 206
+ * Total Accepted:    2.8M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: 2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::cmp;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                cmp::max(
+                    Solution::max_depth(node.borrow().left.clone()),
+                    Solution::max_depth(node.borrow().right.clone()),
+                ) + 1
+            }
+            None => 0,
+        }
+    }
+}
+// @lc code=end
