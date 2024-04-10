280ee5ecd9c77885bb43792f7d6a8d044764048f
Hyden Liu <sunupdong@gmail.com>
2024-04-10 10:23:10 +0800

    Create 145.binary-tree-postorder-traversal.rs

---
+++ src/145.binary-tree-postorder-traversal.rs
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=145 lang=rust
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (70.76%)
+ * Likes:    6778
+ * Dislikes: 185
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = vec![];
+
+        fn traversal(node: &Option<Rc<RefCell<TreeNode>>>, list: &mut Vec<i32>) {
+            if let Some(val) = node {
+                traversal(&val.borrow().left, list);
+                traversal(&val.borrow().right, list);
+                list.push(val.borrow().val);
+            }
+        }
+        traversal(&root, &mut res);
+        res
+    }
+}
+// @lc code=end
